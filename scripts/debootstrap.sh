2e094d4d7bfc2533ecc3f8b7ce0038bc51551950
cameron skene <cameron.t.skene@gmail.com>
2025-09-25 12:40:10 +1000

    bookworm

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 CHROOT=${CHROOT=$(pwd)/rootfs}
-RELEASE=${RELEASE=stable}
+RELEASE=${RELEASE=bookworm}
 HOST_NAME=${HOST_NAME=openstick-debian}
 
 # Download latest Debian archive keyring to fix key verification issues
